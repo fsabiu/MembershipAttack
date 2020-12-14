1eba27fab01be50facc0fdf04a4c81267dbd0fb8
fsabiu <fsabiu@gmail.com>
2020-12-14 12:51:05 +0100

    Lime explainer implementation started

--- explanator.py
+++ explanator.py
@@ -2,11 +2,17 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 class Explanator(ABC):
-    def __init__(self, dataset, class_name, n_classes, black_box):
+    def __init__(self, dataset, class_name, categorical_features, categorical_names, n_classes, black_box):
+        # Data and target
         self.dataset = dataset
         self.class_name = class_name
+        self.categorical_names = categorical_names
+        self.categorical_features = categorical_features
         self.n_classes = n_classes
         self.black_box = black_box
+
+        # Explainer
+        self.explainer = None
 
     @abstractmethod
     def explain(self):
@@ -14,19 +20,43 @@ class Explanator(ABC):
 
 class LoreExplanator(Explanator):
 
-    def __init__(self, dataset, class_name, n_classes, black_box):
+    def __init__(self, dataset, class_name, categorical_features, categorical_names, n_classes, black_box):
         super().__init__(dataset, class_name, n_classes, black_box)
+
 
     def explain(self):
         print("I'm Lore")
 
 class LimeExplanator(Explanator):
-    def __init__(self, dataset, class_name, n_classes, black_box):
+    def __init__(self, dataset, class_name, categorical_features, categorical_names, n_classes, black_box):
         super().__init__(dataset, class_name, n_classes, black_box)
+
+        # Initializing Explainer
+        self.explainer = lime.lime_tabular.LimeTabularExplainer(
+            self.dataset,
+            class_names=[self.class_name],
+            feature_names = self.dataset.columns.remove(self.class_name),
+            categorical_features = self.categorical_features,
+            categorical_names= self.categorical_names,
+            kernel_width = 3,
+            verbose = False)
 
     def explain(self):
         print("I'm Lime")
 
 
-e = LimeExplanator('texas', 'class', '100', 'bb')
+
+black_box = load_obj(folder + 'target/RF/RF_model')
+
+adult = 'mapped_dataset'
+n_classes = 2
+black_box = 'black_box'
+
+
+e = LimeExplanator(dataset = 'adult',
+                class_name = 'class',
+                categorical_features = [],
+                categorical_names = '100',
+                n_classes = 2,
+                black_box = black_box')
 e.explain()
