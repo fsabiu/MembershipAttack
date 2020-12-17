82d8fe56af052446a5c24be466320d0b2c04bdf3
fsabu <fsabiu@gmail.com>
2020-12-17 17:22:59 +0100

    Texas explanation - creation

--- run_explanation.py
+++ run_explanation.py
@@ -134,6 +134,92 @@ def mobility_explain(model_type, bb, r2E):
 
     return neighs
 
+def texas_explain(model_type, bb, r2E):
+
+    n_classes = 100
+
+    dtypes = load_obj('data/texas' + '/dtypes')
+
+    train = pd.read_csv(folder + 'baseline_split/bb_train_mapped.csv', dtype = dtypes, nrows = 100)
+    r2E = pd.read_csv(folder + 'baseline_split/r2E_mapped.csv', dtype = dtypes, nrows = 100)
+
+    dataframe = pd.concat([train, r2E], axis=0)
+
+    print(len(dataframe) == len(train) + len(r2E))
+    # LORE function 'encoded_dataset' -> always returns numeric columns
+    #encoded_data, feature_names, class_values, numeric_columns, rdf, real_feature_names, features_map = encode_dataset(dataframe, 'class')
+
+    print(train.columns)
+
+    print(train.head(5))
+
+    feature_names = list(train.columns)
+    numeric_columns = ['BLOOD_ADM_AMOUNT', 'BLOOD_ADM_AMOUNT', 'ICU_AMOUNT', 'PRIVATE_AMOUNT', 'SEMI_PRIVATE_AMOUNT', 'WARD_AMOUNT', 'ICU_AMOUNT', 'CCU_AMOUNT', 'OTHER_AMOUNT', 'PHARM_AMOUNT', 'MEDSURG_AMOUNT', 'DME_AMOUNT', 'USED_DME_AMOUNT', 'PT_AMOUNT', 'OT_AMOUNT', 'SPEECH_AMOUNT', 'IT_AMOUNT', 'BLOOD_AMOUNT', 'BLOOD_ADMIN_AMOUNT', 'OR_AMOUNT', 'LITH_AMOUNT', 'CARD_AMOUNT', 'ANES_AMOUNT', 'LAB_AMOUNT', 'RAD_AMOUNT', 'MRI_AMOUNT', 'OP_AMOUNT', 'ER_AMOUNT', 'AMBULANCE_AMOUNT', 'PRO_FEE_AMOUNT', 'ORGAN_AMOUNT', 'ESRD_AMOUNT', 'CLINIC_AMOUNT', 'TOTAL_CHARGES', 'TOTAL_NON_COV_CHARGES', 'TOTAL_CHARGES_ACCOMM', 'TOTAL_NON_COV_CHARGES_ACCOMM', 'TOTAL_CHARGES_ANCIL', 'TOTAL_NON_COV_CHARGES_ANCIL']
+
+    print(len(numeric_columns))
+    print(len(feature_names))
+    categorical_features = []
+    for i, feature in enumerate(feature_names):
+        if(feature not in numeric_columns):
+            categorical_features.append(i)
+
+    print(len(categorical_features))
+
+    cat = [c for c in categorical_features if (c not in numeric_columns and c not in feature_names)]
+    print("Cat:")
+    print(cat)
+
+    num = [c for c in numeric_columns if (c not in categorical_features and c not in feature_names)]
+    print("num:")
+    print(num)
+
+
+    class_names = dataframe['PRINC_SURG_PROC_CODE'].unique()
+
+    data = dataframe.to_numpy()
+    train_data = train.to_numpy()
+    #r2E_data = r2E.drop(['class'], axis = 1)
+    r2E_data = r2E.to_numpy()
+
+    categorical_names = {}
+    for feature in categorical_features:
+        le = sklearn.preprocessing.LabelEncoder()
+        le.fit(data[:, feature])
+        data[:, feature] = le.transform(data[:, feature])
+        categorical_names[feature] = le.classes_
+
+
+    data = data.astype(float)
+    train_data = train_data.astype(float)
+    r2E_data = r2E_data.astype(float)
+
+    ex = lime_tabular.LimeTabularExplainer(
+            data,
+            class_names = class_names,
+            feature_names = feature_names,
+            categorical_features = categorical_features,
+            categorical_names = categorical_names,
+            kernel_width = 3,
+            verbose = False,
+            discretize_continuous = False)
+
+
+    predict_fn = lambda x: bb.predict_proba(x).astype(float)
+
+    neighs_dfs = []
+    for i in range(len(r2E)):
+        if(i%1000 == 0):
+            print('Progress: ' + str(i) + '/' + str(len(r2E)))
+
+        data_exp, inverse = ex.explain_instance(r2E_data[i], predict_fn, num_features=5)
+        neighs_dfs.append(pd.DataFrame(data = inverse, columns = r2E.columns).astype(r2E.dtypes))
+
+    neighs = pd.concat(neighs_dfs, axis = 0)
+
+    if (len(neighs) == 5000*len(r2E)):
+        print("Size checking done")
+
+    return neighs
 
 if __name__ == "__main__":
     """
@@ -159,7 +245,9 @@ if __name__ == "__main__":
 
     folder = 'data/' + dataset + '/'
 
-    r2E = pd.read_csv(folder + 'baseline_split/r2E_mapped.csv')
+    r2E = None
+    if(dataset != 'texas'):
+        r2E = pd.read_csv(folder + 'baseline_split/r2E_mapped.csv')
     bb = None
 
     neighs = None
