a5eb60b5d415f0068b688b9844a3db2e3c6203d2
fsabu <fsabiu@gmail.com>
2021-01-05 11:41:33 +0100

    Mean and std attack input

--- attack.py
+++ attack.py
@@ -102,8 +102,8 @@ class Attack(ABC):
         pred_train_target_shaped = np.array([np.array(e) for e in pred_train_target_sample])
         pred_val_target_shaped = np.array([np.array(e) for e in pred_val_target])
         # else:
-        #pred_train_target_shaped = np.array([[np.array(e)] for e in pred_train_target_sample])
-        #pred_val_target_shaped = np.array([[np.array(e)] for e in pred_val_target])
+        #   pred_train_target_shaped = np.array([[np.array(e)] for e in pred_train_target_sample])
+        #   pred_val_target_shaped = np.array([[np.array(e)] for e in pred_val_target])
 
         # Shaping attack validation data
         self.X_val_att = np.vstack((pred_train_target_shaped, pred_val_target_shaped))
@@ -112,11 +112,12 @@ class Attack(ABC):
         self.y_val_att[len(pred_train_target_sample) : len(pred_train_target_sample) + len(pred_val_target)] = 1
 
         # Getting classes from predictions
-        y_train_target_class = np.array([np.argmax(vect) for vect in y_train_target])
-        y_val_target_class = np.array([np.argmax(vect) for vect in y_val_target])
+        y_train_target_class = np.array([self.labels_target[l] for l in y_train_target])
+        y_val_target_class = np.array([self.labels_target[l] for l in y_val_target])
 
-        self.y_val_true =  np.hstack((y_train_target_class[idx], y_val_labels))
+        self.y_val_true =  np.hstack((y_train_target_class[idx], y_val_target_class))
 
+        print(self.y_val_true)
         return
 
     def trainShadowModels(self):
@@ -182,32 +183,19 @@ class Attack(ABC):
         return
 
     def trainAttackModel(self):
-        print("True train labels")
-        print(self.y_true_attack[:20])
-        print("True val labels")
-        print(self.y_val_true[:20])
 
-        print("Target labels")
-        print(self.target_labels[0])
-
-        (unique, counts) = np.unique(self.y_true_attack, return_counts=True)
-        frequencies = np.asarray((unique, counts)).T
-        print(frequencies[:10])
-
-        (unique, counts) = np.unique(self.y_val_true, return_counts=True)
-        frequencies = np.asarray((unique, counts)).T
-        print(frequencies[:10])
+        # Baseline classifier
+        
 
         for i in range(self.n_classes):
             # self.class_indices_train[i] contains indices of X_train_att corresponding to class self.target_labels[i]
-            self.class_indices_train.append([j for j in range(len(self.X_train_att)) if self.y_true_attack[j] == self.target_labels[i] ])
+            self.class_indices_train.append([j for j in range(len(self.X_train_att)) if self.y_true_attack[j] == i ])
 
-            # self.class_indices_val[i] contains indices of X_val_att corresponding to class self.target_labels[i]
-            self.class_indices_val.append([j for j in range(len(self.X_val_att)) if self.y_val_true[j] == self.target_labels[i] ])
+            # self.class_indices_val[i] contains indices of X_val_att corresponding to class i
+            self.class_indices_val.append([j for j in range(len(self.X_val_att)) if self.y_val_true[j] == i ])
 
             if(len(self.class_indices_val[i]) == 0):
                 print("Class " + str(i) + " empty")
-
 
         # Assert sizes of mapping data -> original class
         #print(sum([len(idces) for idces in self.class_indices_train]) == len(self.y_true_attack))
@@ -228,7 +216,7 @@ class Attack(ABC):
             loss = self.attack_params['loss'],
             output_units = 1,
             input_size = self.n_classes)
-
+            print(type(self.attack_models[i]))
             # Permuting train and test
             p_train = np.random.permutation(len(self.class_indices_train[i]))
             p_val = np.random.permutation(len(self.class_indices_val[i]))
@@ -240,13 +228,35 @@ class Attack(ABC):
             y_val = self.y_val_att[self.class_indices_val[i]][p_val]
 
             print("Train shapesss for " + str(i))
-            print(np.shape(X_train))
+            print(np.shape(X_train)) #(1400, 100)
             print(np.shape(X_val))
+
+            means = []
+            stds = []
+            for k in range(self.n_classes):
+                means.append(np.mean(X_train[:,k]))
+                stds.append(np.std(X_train[:,k]))
 
             if(len(X_train) == 0 or len(X_val) == 0):
                 continue
 
+            print("First two X_train")
+            print(X_train[0])
+            print(X_train[1])
+
+            print("First two y_train")
+            print(y_train[0])
+            print(y_train[1])
+
+            print("y_train_frequencies")
+            (unique, counts) = np.unique(y_train, return_counts=True)
+            frequencies = np.asarray((unique, counts)).T
+            print(frequencies)
+
             print("Training attack for class " + str(self.target_labels[i]) + "with train: " + str(len(X_train)) + ", val = " + str(len(X_val)))
+            print("i = " + str(i))
+            print(type(self.attack_models[i]))
+
             self.attack_models[i], self.attack_histories[i] = model_training(self.attack_models[i],
                 X_train,
                 y_train,
@@ -258,8 +268,16 @@ class Attack(ABC):
                 logdir = None)
 
             # Evaluation
-
+            # X_test = ...
+            # y_test = ...
             evaluation = model_evaluation(modelType = 'NN_attack', model = self.attack_models[i], X_val = X_val, y_val = y_val, X_test = X_val, y_test = y_val)
+
+            evaluation['Train_length'] = len(X_train)
+            evaluation['Val_length'] = len(X_val)
+
+            for k in range(self.n_classes):
+                evaluation['mean' + str(k)] = means[k]
+                evaluation['std' + str(k)] = stds[k]
 
             # Logs
             self.attack_params['target_class'] = self.target_labels[i]
